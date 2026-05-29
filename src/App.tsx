307d21a03d1d49634902609e1cea2d502cf52d16
valisoa01 <valisoatolotriniaina@gmail.com>
2026-05-29 21:45:30 +0300

    add create emp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
-
 import { EmployeeList } from "./employees/EmployeeList";
+import { EmployeeCreate } from "./employees/EmployeeCreate";
 
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
@@ -9,6 +9,7 @@ function App() {
   return (
     <Admin dataProvider={dataProvider}>
       <Resource name="employees" list={EmployeeList} />
+      <Resource name="employees" list={EmployeeList} create={EmployeeCreate} />
     </Admin>
   );
 }
